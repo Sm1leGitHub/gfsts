94d38b43d69d826b06b0b20949339b7f46836f37
Sm1leGitHub <113791071+Sm1leGitHub@users.noreply.github.com>
2022-10-20 18:43:22 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+int main() {
+	/*int i = 10;
+	while (i <= 20) {
+		if (i % 3 == 0) {
+			cout << i << ' ';
+		}
+		i++;*/
+
+	/*18
+	int N;
+	cin >> N;
+	int i = 1;
+	int k = 0;
+	while(i<=N){
+		if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0) {
+			k++;
+		}
+		i++;
+	}
+	cout << k;*/
+	
+	/*19
+	int a, b;
+	for (int a = 10; a < 100;a++){
+		int k = a * 100;
+			for (int b = 10; b < 100; b++) {
+				int m = k + b;
+				int proiz = a * b;
+				if (m % proiz == 0) {
+					cout << m<<' ';
+				}
+			}
+	}*/
+
+	
+	/*float n;
+	float i = 2;
+	float sum = 0;
+	cin >> n;
+	while (i<=n ) {
+		sum = sum + (1 /(2*( i * i)));
+		i += 1;
+	}
+	cout << sum;???????????????????????????????????????????????????????????????????????????????????????????*/
+
+	/*22
+	float sum = 0;
+	int n;
+	int i = 1;
+	cin >> n;
+	while (i <= n) {
+		sum = sum + 1.0 / i ;
+		i += 1;
+	}
+	cout << sum;*/
+
+	/*int i = 1;
+	int sum = 0;
+	while (i <= 1024) {
+		sum = sum + i;
+		i *= 2;
+	}
+	cout << sum;*/
+
+
+	
+}
